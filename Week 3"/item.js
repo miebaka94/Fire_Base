728e6388fc534c3cae99800f1ebeae1fa20f336e
Miebaka Semenitari <130510170+miebaka94@users.noreply.github.com>
2024-07-29 13:02:49 -0600

    Add files via upload

---
+++ Week 3"/item.js
@@ -0,0 +1,9 @@
+export default function item({name, quantity, category}) {
+  return (
+    <main className="bg-pink-200 p-4 rounded-md ">
+      <div className="font-bold text-lg">{name}</div>
+      <div className="text-gray-600">Quantity: {quantity}</div>
+      <div className="text-gray-600">Category: {category}</div>
+    </main>
+  );
+};

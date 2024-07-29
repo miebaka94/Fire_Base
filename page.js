5e08751743509e7e6712752c9320a68296e5acca
Miebaka Semenitari <130510170+miebaka94@users.noreply.github.com>
2024-07-29 13:01:06 -0600

    Delete page.js

--- page.js
+++
@@ -1,9 +0,0 @@
-import ItemList from "./item-list"
-export default function page() {
-  return (
-    <main>
-     <h1 className="text-4xl font-serif">Shopping List</h1>
-      <ItemList/>
-    </main>
-  )
-}

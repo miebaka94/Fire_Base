a8ddca934cd13e9dbca217194652c765b5143d88
Miebaka Semenitari <130510170+miebaka94@users.noreply.github.com>
2024-07-29 13:19:23 -0600

    Update page.js

--- week-3/page.js
+++ week-3/page.js
@@ -1,1 +1,11 @@
 
+import ItemList from "./item-list"
+export default function page() {
+  return (
+    <main>
+     <h1 className="text-4xl font-serif">Shopping List</h1>
+      <ItemList/>
+    </main>
+  )
+}
+

5a0d0dc2d4fa71e02720b63285ece5f558092937
Miebaka Semenitari <130510170+miebaka94@users.noreply.github.com>
2024-07-29 12:58:29 -0600

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+import ItemList from "./item-list"
+export default function page() {
+  return (
+    <main>
+     <h1 className="text-4xl font-serif">Shopping List</h1>
+      <ItemList/>
+    </main>
+  )
+}

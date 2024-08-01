241477226bf56162e37358f9497286d05df8dfd6
Miebaka Semenitari <130510170+miebaka94@users.noreply.github.com>
2024-07-31 20:52:44 -0600

    Delete Week 3" directory

--- Week 3"/item.js
+++
@@ -1,9 +0,0 @@
-export default function item({name, quantity, category}) {
-  return (
-    <main className="bg-pink-200 p-4 rounded-md ">
-      <div className="font-bold text-lg">{name}</div>
-      <div className="text-gray-600">Quantity: {quantity}</div>
-      <div className="text-gray-600">Category: {category}</div>
-    </main>
-  );
-};

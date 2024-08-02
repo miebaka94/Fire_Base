69f0f7fca0c9d9b6140ae6b678af954dfecb84ff
Miebaka Semenitari <130510170+miebaka94@users.noreply.github.com>
2024-08-02 13:17:04 -0600

    Update item.js

--- week-5/item.js
+++ week-5/item.js
@@ -1,1 +1,10 @@
+export default function item({name, quantity, category}) {
+  return (
+    <main className="bg-gray-200 p-4 rounded-md ">
+      <div className="font-bold text-lg">{name}</div>
+      <div className="text-gray-600">Quantity: {quantity}</div>
+      <div className="text-gray-600">Category: {category}</div>
+    </main>
+  );
+};
 

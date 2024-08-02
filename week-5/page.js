c32cbdcab8e71a7ad98f79adbdeddfd787444072
Miebaka Semenitari <130510170+miebaka94@users.noreply.github.com>
2024-08-02 13:31:47 -0600

    Update page.js

--- week-5/page.js
+++ week-5/page.js
@@ -2,9 +2,8 @@ import ItemList from "./item-list"
 
 export default function page() {
   return (
-    <main>
-      <h1>Shopping List</h1>
-      <ItemList />
-    </main>
-  );
+  <div>
+    <NewItem/>
+  </div>
+  )
 }

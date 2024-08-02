70cafe8c2a2038693c0c9f451333aa6302ad0f4c
Miebaka Semenitari <130510170+miebaka94@users.noreply.github.com>
2024-08-02 12:34:45 -0600

    Update page.js

--- week-2/page.js
+++ week-2/page.js
@@ -1,1 +1,12 @@
+import Link from "next/link";
 
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Miebaka Semenitari</h1>
+      <Link href="https://github.com/miebaka94" passHref>
+      https://github.com
+      </Link>
+    </div>
+  );
+}

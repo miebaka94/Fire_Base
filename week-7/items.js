8c54b8c399d6277b3aa3142a309b97e3a2f5bedb
Miebaka Semenitari <130510170+miebaka94@users.noreply.github.com>
2024-08-28 12:03:15 -0600

    Delete week-7 directory

--- week-7/items.js
+++
@@ -1,8 +0,0 @@
-export default function Item({name, quantity, category}) {
-    return (
-      <li className="border p-4 mb-2   bg-gray-800 border-gray-700 rounded text-white">
-        <p className="font-bold capitalize">{name}</p>
-        <p>Buy {quantity} in {category}</p>
-      </li>
-    );
-  }
